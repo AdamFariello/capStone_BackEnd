8d35cecd6ad7c8d667bb7fa4eeaac45fd487253b
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-10-21 05:24:59 -0400

    Created the delete route

--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -1,5 +1,8 @@
 import express from "express";
+import error from "../middleware/errors.mjs"
 import db from "../db/conn.mjs";
+
+import usersSchema from "../db/models/usersSchema.mjs";
 
 const router = express.Router();
 let userColl = db.collection("user");
@@ -22,6 +25,28 @@ router.route("/")
           console.error(e.message);
           next(error(400, e))
         }
+      })
+      .delete(async(req, res, next) => {
+        if (req.body.username && req.body.email && req.body.password) {
+          let query = {
+            username: String(req.body.username),
+            email: String(req.body.email),
+            password: String(req.body.password)
+          }
+                    
+          let result = await userColl.deleteOne(query);
+
+          if (result["deletedCount"]) {
+            res.json(result);
+          } else {
+            next(error(400, "Error occured, user wasn't deleted or doesn't exist"));
+          }
+        } else {
+          next(next(400, "ERROR: missing username, password, and/or email"));
+        }
+      })
+      .patch(async(req, res, next) => {
+
       })
 ;
 
