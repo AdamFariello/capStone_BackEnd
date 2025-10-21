0339d2a6b61b02a95d4d2af6047979fdc853f1c7
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-10-20 22:28:34 -0400

    Moved the test insert into its own test function

--- initDatabase.mjs
+++ initDatabase.mjs
@@ -1,8 +1,6 @@
 import db from "./db/conn.mjs";
 
-
-
-export default async function initDatabase() {
+async function testInsert() {
     let testCol = await db.collection("test2");
     try {
         await db.createCollection("test2");
@@ -10,6 +8,12 @@ export default async function initDatabase() {
         console.error(e.message);
     }
     
-    testCol.deleteMany({}); // Empties table
-    await testCol.insertOne({example: "entry", alternative: "entry2"});
+    await testCol.deleteMany({}); // Empties table
+    await testCol.insertOne({bogo: "printed", uhWhat: "👽"}); 
+    
+    console.log("Successful run");
+} 
+
+export default async function initDatabase() {
+    //testInsert();
 }
