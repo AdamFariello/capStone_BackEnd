163f450b6db3b6d721e10e1d1c942bb479fb4f45
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-10-21 05:08:37 -0400

    Added POST route

--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -6,9 +6,32 @@ let userColl = db.collection("user");
 
 router.route("/")
       .get(async(req, res) => {
-        let getUsers = await userColl.find({}).toArray();
+        let getUsers;
+        if (req.query.user) {
+          getUsers = await userColl.find({"username": req.query.user}).toArray();
+        } else {
+          getUsers = await userColl.find({}).toArray();
+        }
         res.json(getUsers);
       })
+      .post(async(req, res, next) => {
+        try {
+          await userColl.insertOne(req.body);
+          res.json(req.body);
+        } catch (e) {
+          console.error(e.message);
+          next(error(400, e))
+        }
+      })
+;
+
+router.route("/:user")
+      .get(async(req, res) => {
+        let getUsers = await userColl.find({"username": req.params.user}).toArray();
+        res.json(getUsers);
+      })
+;
+
 
 
 export default router;
