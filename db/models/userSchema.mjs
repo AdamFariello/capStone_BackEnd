a343b0519055aae5e85f2740815b48f57b291122
AdamFariello <ajf270@scarletmail.rutgers.edu>
2025-10-21 01:51:59 -0400

    Created userSchema

---
+++ db/models/userSchema.mjs
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const usersSchema = new mongoose.Schema({
+    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+});
+export default mongoose.model("Users", usersSchema); 
